4b0c2b55a6fc8dfcb94da81773116b2e49bf10f1
Avik816 <manikchakrabarti745@gmail.com>
2025-07-13 11:36:19 +0530

    Create config.py

---
+++ src/CONFIG.py
@@ -0,0 +1,15 @@
+# Paths
+RAW_TRAIN_DIR = 'dataset/train.csv'
+RAW_VAL_DIR = 'dataset/val.csv'
+RAW_TEST_DIR = 'dataset/test.csv'
+RAW_FULL_DATASET_DIR = 'raw datasets/Full news dataset.csv'
+CLEANED_FULL_DATASET_DIR = 'raw datasets/Full news dataset.csv'
+
+TRAIN_DIR = 'data/train.csv'
+VAL_DIR = 'data/val.csv'
+TEST_DIR = 'data/test.csv'
+
+# Model Parameters
+TRAIN_SIZE = 0.80
+VAL_SIZE = 0.10
+TEST_SIZE = 0.10
