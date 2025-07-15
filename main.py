5b7806c2e5d25f93ac9e816cf450069e29a3b6ee
Avik816 <manikchakrabarti745@gmail.com>
2025-07-15 23:58:07 +0530

    Added module calls

---
+++ main.py
@@ -0,0 +1,15 @@
+import src
+
+print('DOWNLOADING DATASET ...')
+src.download_dataset()
+
+print('MAKING FULL DATASET ...')
+src.make_full_dataset()
+
+print('PREPROCESSING DATASET ...')
+src.preprocess_dataset()
+
+print('MAKING DATASET FOR THE MODEL ...')
+src.make_dataset_for_model()
+
+print('MODEL TRAINING ...')
