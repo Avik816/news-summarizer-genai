aff3b80804ce2fdf560bb7b66b456bfd57fd31f3
Avik816 <manikchakrabarti745@gmail.com>
2025-07-18 13:59:57 +0530

    Added tokenizer as utils

--- src/utils/tokenizer.py
+++ src/utils/tokenizer.py
@@ -1,9 +1,14 @@
+import os
+from ..CONFIG import TOKENIZER_PATH, MODEL_NAME
 from transformers import T5Tokenizer
-from ..CONFIG import MODEL_NAME
 
 
 def get_tokenizer():
     # Downloading and setting up the tokenizer
-    tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+    if os.path.exists(TOKENIZER_PATH):
+        tokenizer = T5Tokenizer.from_pretrained(TOKENIZER_PATH)
+    else:
+        tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+        tokenizer.save_pretrained(TOKENIZER_PATH)
 
     return tokenizer
