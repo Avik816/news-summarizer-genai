284fc5a306334f45bf9400f99288fa6ee1079f2e
Avik816 <manikchakrabarti745@gmail.com>
2025-08-12 21:30:28 +0530

    Using t5 tokenizer fast

--- src/utils/tokenizer.py
+++ src/utils/tokenizer.py
@@ -1,14 +1,14 @@
 import os
 from ..CONFIG import TOKENIZER_PATH, MODEL_NAME
-from transformers import T5Tokenizer
+from transformers import T5TokenizerFast
 
 
 def get_tokenizer():
     # Downloading and setting up the tokenizer
     if os.path.exists(TOKENIZER_PATH):
-        tokenizer = T5Tokenizer.from_pretrained(TOKENIZER_PATH)
+        tokenizer = T5TokenizerFast.from_pretrained(TOKENIZER_PATH)
     else:
-        tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+        tokenizer = T5TokenizerFast.from_pretrained(MODEL_NAME)
         tokenizer.save_pretrained(TOKENIZER_PATH)
 
     return tokenizer
