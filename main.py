cd8249678aedc2664723fe93b808a606e559f5a7
Avik816 <manikchakrabarti745@gmail.com>
2025-08-20 23:59:43 +0530

    Updated accordingly

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import src
 
 warnings.filterwarnings('ignore')
 
-"""print('DOWNLOADING DATASET ...')
-src.download_dataset()
+if __name__ == '__main__':
+    """print('DOWNLOADING DATASET ...')
+    src.download_dataset()
 
-print('MAKING FULL DATASET ...')
-src.make_full_dataset()
+    print('MAKING FULL DATASET ...')
+    src.make_full_dataset()
 
-print('PREPROCESSING DATASET ...')
-src.preprocess_dataset()
+    print('PREPROCESSING DATASET ...')
+    src.preprocess_dataset()"""
 
-print('MAKING DATASET FOR THE MODEL ...')
-src.make_dataset_for_model()"""
+    print('MAKING DATASET FOR THE MODEL ...')
+    src.make_dataset_for_model()
 
-print('MODEL TRAINING ...')
-src.train_T5Small_model()
+    print('MODEL TRAINING ...')
+    src.train_T5Small_model()
